ceecaa4b5b3264a8488f0419361d5c4f03bfaf2b
mhhkia <mhhk1983@gmail.com>
2025-08-19 12:43:15 +0200

    Avacado and Dancing still not appearing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
 
     public void NextChallenge()
     {
-        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        GameObject[] allObjects = FindObjectsByType<GameObject>(FindObjectsSortMode.None);
 
         foreach (GameObject obj in allObjects)
         {
